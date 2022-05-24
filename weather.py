efc95eec5a25a50103d627a34a6e887148110b51
demidraksin <greatraksin@icloud.com>
2022-05-24 19:11:24 +0300

    Finishing get_weather function

--- weather.py
+++ weather.py
@@ -1,18 +1,33 @@
 import requests as r
-import json
+import os
+from datetime import datetime
 
 
 def get_weather(city):
     url = 'https://api.openweathermap.org/data/2.5/forecast'
     params = {  # параметры для запроса
         'q': city.capitalize(),  # Пишет город с большой буквы
-        'appid': '',
+        'appid': os.environ.get('WEATHER_KEY'),
         'units': 'metric',  # градусы в цельсиях
         'lang': 'ru'  # русский язык
     }
     response = r.get(url, params=params)
-    return response.json()  # возвращаю ответ сервера в JSON формате
+    if response.status_code == 200:
+        data = response.json()
+        today = datetime.today()  # сегодняшняя дата и время
+        forecast = []  # здесь буду хранить прогнозы
+        for line in data['list']:
+            date = datetime.fromtimestamp(line['dt'])
+            if date.day in [today.day, today.day + 1]:
+                day = {
+                    'date': datetime.strftime(date, '%d/%m, %H:%M'),
+                    'temp': round(line['main']['temp']),
+                    'weather': line['weather'][0]['description']
+                }
+                forecast.append(day)
+        return forecast
+    else:
+        return None
 
-with open('weather.json', 'w') as file:
-    w = get_weather('Минск')
-    file.write(json.dumps(w))
+
+print(get_weather('Гонолулу'))
